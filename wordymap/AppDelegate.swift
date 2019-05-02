2c3156acbf5dd532fe9dabc6d529d6c35b9e48d0
jbbe <jbbe@umich.edu>
2019-05-02 14:55:57 -0400

    Initial Commit

---
+++ wordymap/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  wordymap
+//
+//  Created by Joanna Bell on 5/2/19.
+//  Copyright © 2019 Joanna Bell. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
